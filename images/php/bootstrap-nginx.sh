1a4fd72da22bad43e20aa25f64ef621cb2ba7450
Robin Appelman <robin@icewind.nl>
2021-04-08 15:42:03 +0200

    s3cmd

--- images/php/bootstrap-nginx.sh
+++ images/php/bootstrap-nginx.sh
@@ -10,7 +10,7 @@ GID=${GID:-1000}
 
 groupadd -g $GID haze
 useradd -m -u $UID -g $GID haze
-chown haze:haze /home/haze
+chown -R haze:haze /home/haze
 
 /usr/local/sbin/php-fpm &
 /etc/init.d/nginx start
