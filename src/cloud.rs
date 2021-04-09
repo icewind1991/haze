68d44b0039cc5eb874b06c476a00fcae1158b134
Robin Appelman <robin@icewind.nl>
2021-04-09 20:12:20 +0200

    no hard error when datadir cant be deleted
    its probably gone already

--- src/cloud.rs
+++ src/cloud.rs
@@ -261,9 +261,12 @@ impl Cloud {
             .remove_network(&self.network)
             .await
             .wrap_err("Failed to remove network")?;
-        remove_dir_all(self.workdir)
+        if let Err(e) = remove_dir_all(self.workdir)
             .await
-            .wrap_err("Failed to remove work directory")?;
+            .wrap_err("Failed to remove work directory")
+        {
+            eprintln!("{}", e);
+        }
 
         Ok(())
     }
