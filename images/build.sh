406bbbc687cb09e1a4a1599678af73e4b5c817e0
Robin Appelman <robin@icewind.nl>
2021-12-15 16:06:59 +0100

    minimize dockerfile diffs

--- images/build.sh
+++ images/build.sh
@@ -4,7 +4,7 @@ versions=("7.2" "7.3" "7.4" "8.0")
 
 for version in "${versions[@]}"; do
   echo "building haze-php-$version"
-  docker build -t "icewind1991/haze-php:$version" -f "php/Dockerfile.$version" php
+  docker build --build-arg PHP_VERSION=$version -t "icewind1991/haze-php:$version" -f "php/Dockerfile.$version" php
 done
 
 for version in "${versions[@]}"; do
