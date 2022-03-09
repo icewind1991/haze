15c430abeeb6cdf1fe320d87eefbc333be91f8a2
Robin Appelman <robin@icewind.nl>
2022-03-09 23:50:07 +0100

    php with debug

--- images/build.sh
+++ images/build.sh
@@ -4,7 +4,7 @@ versions=("7.3" "7.4" "8.0" "8.1")
 
 for version in "${versions[@]}"; do
   echo "building haze-php-$version"
-  docker build --build-arg BASE_IMAGE=icewind1991/php-dbg PHP_VERSION=$version -t "icewind1991/haze-php:$version" -f "php/Dockerfile" php
+  docker build --build-arg BASE_IMAGE=icewind1991/php-dbg --build-arg PHP_VERSION=$version -t "icewind1991/haze-php:$version" -f "php/Dockerfile" php
 done
 
 for version in "${versions[@]}"; do
