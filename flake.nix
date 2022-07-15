59e9bb3631ed64a4a12e665e561aed2db28b4647
Robin Appelman <robin@icewind.nl>
2022-07-15 13:44:58 +0200

    basic flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, naersk }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages."${system}";
+        naersk-lib = naersk.lib."${system}";
+      in
+        rec {
+          # `nix build`
+          packages.haze = naersk-lib.buildPackage {
+            pname = "haze";
+            root = ./.;
+          };
+          defaultPackage = packages.haze;
+          defaultApp = packages.haze;
+
+          # `nix develop`
+          devShell = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [ rustc cargo bacon ];
+          };
+        }
+    );
+}
